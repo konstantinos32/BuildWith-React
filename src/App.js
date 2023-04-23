d248d26d5f853c3489fb7799fd568c4c89da8952
Konstantinos Theodoropoulos <konstantinostheodoropoulosphoto@hotmail.com>
2023-04-23 12:52:01 +0200

    App basic stracture is with visible components in the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import {Footer, Blog, Features, Possibility, WhatGPT3, Header, } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 
 const App = () => {
-  return (<div>
-    <h1>GPT-3</h1>
-  </div>
+  return (
+    <div className="App">
+      <div className = "gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 };
 
-export default App;
+export default App

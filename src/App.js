185812d9814bbb35b6395ac40aed23ee7b3ece4b
Konstantinos Theodoropoulos <konstantinostheodoropoulosphoto@hotmail.com>
2023-04-24 14:18:04 +0200

    App.css basic set up 🚀

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Footer, Blog, Features, Possibility, WhatGPT3, Header, } from "./containers";
-import { CTA, Brand, Navbar } from "./components";
+import {Footer, Blog, Features, Possibility, WhatGPT3, Header, } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (

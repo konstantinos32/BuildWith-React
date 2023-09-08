dd3c97f055be910591b8511d4a52f7f52fa46040
konstantinos32 <76957273+konstantinos32@users.noreply.github.com>
2023-09-08 03:18:22 +0200

    finished app 😋😋

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Feature } from '../../components';
-import "./features.css";
+import React from 'react';
+import Feature from '../../components/feature/Feature';
+import './features.css';
 
 const featuresData = [
   {
-    title: ' Improving end distrusts instantly ',
+    title: 'Improving end distrusts instantly',
     text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
   },
   {
@@ -16,31 +16,23 @@ const featuresData = [
     text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
   },
   {
-    title: 'Really boy law country',
-    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.',
+    title: 'Really boy law county',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..',
   },
-   
+];
 
-  
-]
-
-const Features = () => {
-  return (
-  <div className = "gpt3__features_section__padding" id="features">
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
     <div className="gpt3__features-heading">
-      <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen</h1>
+      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
       <p>Request Early Access to Get Started</p>
     </div>
     <div className="gpt3__features-container">
-       {featuresData.map((item, index) => (
-         <Feature  title={item.title} text={item.text} key={item.title + index}  />
-       ))}
-
-  
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
     </div>
   </div>
-  )
-}
+);
 
-
-export default Features
+export default Features;
